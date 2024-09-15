37eb5ab59a7513ffe1728efc058fdcf0ca46263f
Teeraphat Prannok <teeraphat1325@gmail.com>
2024-09-15 18:33:23 +0700

    innitial project

---
+++ src/main/java/com/mycompany/databaseproject/DatabaseProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.databaseproject;
+
+/**
+ *
+ * @author teeraphat
+ */
+public class DatabaseProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
