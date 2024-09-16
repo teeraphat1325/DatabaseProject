3132834e0206dae455beb72f7da8a5b6cb4eb52a
Teeraphat Prannok <teeraphat1325@gmail.com>
2024-09-16 18:34:16 +0700

    add Dao.java and UserDao.java in dao package

---
+++ src/main/java/com/mycompany/databaseproject/dao/Dao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.databaseproject.dao;
+
+/**
+ *
+ * @author informatics
+ */
+public interface Dao<T> {
+    T get(int id);
+    List<T> getAll();
+    T save(T obj);
+    T update(T obj);
+    int delete(T obj);
+    
+}
